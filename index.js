13cdcac5937e7a07ddfdc3c04589b228d34199a7
Viviane Viana Bresolin <vivianebresolin@gmail.com>
2023-07-14 14:20:33 -0400

    Add comments and make some adjustments

--- index.js
+++ index.js
@@ -10,31 +10,34 @@ const URL = 'https://www.fsu.ca/jobs';
 const text = 'There are no current Hospitality job postings.'
 let title = '';
 let siblingText = '';
-let myResponse = 'No changes found.';
+let myResponse = '';
 
 const app = express();
-app.listen(PORT, () => console.log('Server running on PORT ' + PORT) );
+// Start the server
+app.listen(PORT, () => console.log('Server running on port ' + PORT) );
 
+// Function to send an email notification
 function sendEmail(data) {
   const email = process.env.EMAIL;
   const emailPassword = process.env.EMAIL_PASSWORD;
   
-  // Configure your email transport
+  // Configure the email transport
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
-      user: email, // Replace with your email address
-      pass: emailPassword, // Replace with your email password or app password
+      user: email,
+      pass: emailPassword,
     },
   });
 
   const mailOptions = {
-    from: email, // Replace with your email address
-    to: email, // Replace with the recipient's email address
+    from: email,
+    to: email,
     subject: '[FSU] Web Scraper Alert',
     text: `${data}`,
   };
 
+  // Send the email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error('Error sending email:', error);
@@ -44,6 +47,7 @@ function sendEmail(data) {
   });
 }
 
+// Function to scrape the website and compare results
 async function scrapeWebsite() {
   const now = new Date();
   const currentDateTime = now.toLocaleString();
@@ -54,24 +58,28 @@ async function scrapeWebsite() {
     const scrapedData = $('h3:contains("Hospitality")');
     
     scrapedData?.each((i, element) => {
-      title = $(element).text(); // H3 text
-      siblingText = $(element).next().text(); // Text from the H3 next sibling (it can be a <span> or a list(<ul>))
+      title = $(element).text(); // In my example, it is an H3 text
+      siblingText = $(element).next().text(); // In my example, it is a text from the H3 next sibling (it can be a <span> or a list(<ul>))
     });
 
     if(siblingText !== text){
-      myResponse = `Title: ${title}, Text: ${siblingText}`;
+      myResponse = `Title: ${title}, Text: ${siblingText}. [${currentDateTime}]`;
       sendEmail(myResponse);
     }
 
-    app.get('/', (req, res) => {
-      res.send(`${myResponse}  ${currentDateTime}`);
-    });
+    myResponse = `No changes found. [${currentDateTime}]`;
     
-    console.log(`${myResponse}  ${currentDateTime}`);
+    console.log(myResponse);
   } catch(error) {
     console.log(`Error scraping website: ${error}`)
   }
 }
+
+// Express route to trigger the scraper manually
+app.get('/', (_, res) => {
+  scrapeWebsite();
+  res.send(myResponse);
+});
 
 // Schedule the scraping task to run every hour
 // https://www.netiq.com/documentation/cloud-manager-2-5/ncm-reference/data/bexyssf.html
